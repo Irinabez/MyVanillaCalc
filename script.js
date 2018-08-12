5370b42d0997f9fbee3751048e368a5fc543f4d5
Irina Bezrodnaya <irinabez@gmail.com>
2018-08-12 14:54:42 -0700

    Shows intermediate results if continue doing opreations without pressing result

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let firstValue= '';
 let operandValue= '';
 let secondValue= '';
+let result = 0;
 
 function digit(n) {
-    console.log(`Works! ${n}`);
+
 
     if (operandValue) {
         secondValue +=n;
@@ -17,31 +18,47 @@ function digit(n) {
 function write(n) {
     document.getElementById('display').value = n;
 }
+function read(n) {
+    n = document.getElementById('display').value;
+}
 
 function operand(operand) {
     console.log (`operand ${operand}`);
-    write('');
+    if (firstValue && secondValue && operandValue) getResult();
     operandValue = operand;
 }
+
 function getResult() {
-    let result=0;
+    result=0;
 
     if(operandValue === '+') {
-        result = (+firstValue + +secondValue);
+        result = +firstValue + +secondValue;
     }
-    if(operandValue === '+') {
-        result = (+firstValue + +secondValue);
+    if(operandValue === '-') {
+        result = +firstValue - +secondValue;
     }
-    if(operandValue === '+') {
-        result = (+firstValue + +secondValue);
+    if(operandValue === '*') {
+        result = +firstValue * +secondValue;
     }
-        write(result);
-        setInitialValue(result);
+    if(operandValue === '/') {
+        result = +firstValue / +secondValue;
+    }
 
-
+    write(result);
+    setInitialValue(result);
 }
+
 function setInitialValue(firstValueByDefault) {
     firstValue = firstValueByDefault;
     operandValue = '';
     secondValue = '';
+}
+
+
+function allClear() {
+    //result=0;
+    firstValue= '';
+    operandValue= '';
+    secondValue= '';
+    write(0);
 }
